220f2b93e987df0ead1bb3a7f09009ee9f2089d5
Mouad <84838650+modoben98@users.noreply.github.com>
2024-10-09 16:06:37 +0100

    Add U.I to the rock paper scisor page

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-let humanScore = 0 ;
+let userScore = 0 ;
 let computerScore = 0 ;
 
 
@@ -12,25 +12,25 @@ function getComputerChoice() {
 	}
 
 	else {
-		return 'cisor' ;
+		return 'scisor' ;
 	}
 }
 
 function getUserChoice() {
-	let user_choice = prompt("Rock, Paper, Cisor").toLowerCase() ;
+	let user_choice = prompt("Rock, Paper, Scisor").toLowerCase() ;
 
-	if (user_choice == "rock" || user_choice == "paper" || user_choice == "cisor") {
+	if (user_choice == "rock" || user_choice == "paper" || user_choice == "scisor") {
 		return user_choice ;
 	}
 
 	else {
-		console.log("Please write your choice between rock, paper and cisor") ;
+		console.log("Please write your choice between rock, paper and scisor") ;
 	}	
 }
 
 
-function playRound() {
-	let user_choice = getUserChoice() ;
+function playRound(user_choice) {
+	
 	let computer_choice = getComputerChoice() ;
 
 	switch (true) {
@@ -42,44 +42,59 @@ function playRound() {
 	case (user_choice == "paper" && computer_choice == "paper") :
 		console.log("Draw !") ;
 		computerScore += 0.5 ;
-		humanScore += 0.5;
+		userScore += 0.5;
 		break ;
 
-	case (user_choice == "cisor" && computer_choice == "paper") :
-		console.log("You Win ! Cisor beats paper") ;
-		humanScore += 1 ;
+	case (user_choice == "scisor" && computer_choice == "paper") :
+		console.log("You Win ! SCisor beats paper") ;
+		userScore += 1 ;
 		break ;
 
 	case (user_choice == "rock" && computer_choice == "rock") :
 		console.log("Draw !") ;
 		computerScore += 0.5 ;
-		humanScore += 0.5;
+		userScore += 0.5;
 		break ;
 
 	case (user_choice == "paper" && computer_choice == "rock") :
 		console.log("You Win ! Paper beats rock") ;
-		humanScore += 1;
+		userScore += 1;
 		break ;
 
-	case (user_choice == "cisor" && computer_choice == "rock") :
-		console.log("You lose ! rock beats cisor") ;
+	case (user_choice == "scisor" && computer_choice == "rock") :
+		console.log("You lose ! rock beats scisor") ;
 		computerScore += 1 ;
 		break ;
 
-	case (user_choice == "rock" && computer_choice == "cisor") :
-		console.log("You Win ! rock beats cisor") ;
-		humanScore += 1;
+	case (user_choice == "rock" && computer_choice == "scisor") :
+		console.log("You Win ! rock beats scisor") ;
+		userScore += 1;
 		break ;
 
-	case (user_choice == "paper" && computer_choice == "cisor") :
-		console.log("You Lose! cisor beats paper") ;
+	case (user_choice == "paper" && computer_choice == "scisor") :
+		console.log("You Lose! Scisor beats paper") ;
 		computerScore += 1 ;
 		break ;
 
-	case (user_choice == "cisor" && computer_choice == "cisor") :
+	case (user_choice == "scisor" && computer_choice == "scisor") :
 		console.log("Draw !") ;
 		computerScore += 0.5 ;
-		humanScore += 0.5;
+		userScore += 0.5;
 		break ;
 	}
 }
+
+
+const buttons = document.querySelectorAll("button");
+
+const userScoreNode = document.querySelector(".user .score")
+const computerScoreNode = document.querySelector(".computer .score")
+
+buttons.forEach((button) => {
+	button.addEventListener("click", function() {
+		playRound(button.id);
+		userScoreNode.textContent = userScore;
+		computerScoreNode.textContent = computerScore;
+	});
+})
+
